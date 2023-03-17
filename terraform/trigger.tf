baefbc96a65f4af24a33f93ebade011446689684
dongboyang00 <dongboyang00@gmail.com>
2023-03-17 15:45:18 -0600

    Deploy hello world to GKE

--- terraform/trigger.tf
+++ terraform/trigger.tf
@@ -1,11 +1,3 @@
-resource "google_project_service" "service" {
-    service =  "run.googleapis.com"
-
-    project            = var.project_id
-    disable_on_destroy = false
-}
-
-
 resource "google_cloudbuild_trigger" "deploy_trigger" {
     name = var.service
 
